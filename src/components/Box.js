67ea810ec0d62ddbb2e08fde913783042b460734
Kyoo130 <kml9003@kakao.com>
2022-04-22 23:27:32 +0900

    :sparkles: 가위바위보 결과 표시 기능 개발

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Box = (props) => {
+  let result;
+  if (
+    props.title === "Computer" &&
+    props.result !== "tie" &&
+    props.result !== ""
+  ) {
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    result = props.result;
+  }
+
   return (
     <div className="box">
       <h1>{props.title}</h1>
-      <img className="item-img"
-           src={props.item && props.item.img}
-           alt="가위바위보이미지"/>
-      <h2>Win</h2>
+      <img
+        className="item-img"
+        src={props.item && props.item.img}
+        alt="가위바위보이미지"
+      />
+      <h2>{result}</h2>
     </div>
   );
 };
